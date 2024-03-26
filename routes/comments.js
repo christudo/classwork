6f42b9a9d18cc610ecc6cabbc39017beba811ecd
Christine DaCruz <cdacruz@pratt.edu>
2024-03-26 16:40:49 -0400

    Label Capitilization

--- routes/comments.js
+++ routes/comments.js
@@ -13,6 +13,7 @@ router.get('/new', function(req, res, next) {
   res.render('comments/new', { title: 'Add Comment' });
 });
 
+
 router.post('/', function(req, res, next) {
   const newComment = req.body;
   Comment.upsert(newComment);
@@ -41,27 +42,3 @@ router.get('/:id/comments', (req, res) => {
 });
 
 module.exports = router;
-
-/*
-router.post('/upsert', async (req, res, next) => {
-  console.log('body: ' + JSON.stringify(req.body));
-  Comment.upsert(req.body);
-  let createdOrUpdated = req.body.id ? 'updated' : 'created';
-  req.session.flash = {
-    type: 'info',
-    intro: 'Success!',
-    message: `The comment has been ${createdOrUpdated}!`,
-  };
-  res.redirect(303, '/comments')
-});
-
-router.get('/show/:commentId', function(req, res, next) {
-  const commentId = req.params.commentId;
-  const books = Book.all.filter(book => book.commentId === commentId);
-  const comment = Comment.get(commentId);
-  res.render('comments/show', { title: 'Comment Books', commentId: commentId, books: books });
-});
-
-
-module.exports = router;
-*/
